5fd7acdaed7bd81c9fb462cfc23f0c4958f66e32
James Burnett <james.c.burnett@icloud.com>
2024-02-25 10:25:16 -0500

    should be done now :D

--- TheBetterSpline/SplineTesting.java
+++ TheBetterSpline/SplineTesting.java
@@ -7,7 +7,7 @@ public class SplineTesting {
 
         Spline spline = new Spline(new Point(52.4, 34.2), 0.5, new RobotPose(0, 0, 0));
 
-        spline.update(new RobotPose(-60.5, 41.3, 0));
+        spline.update(new RobotPose(-86.0, 88.4, 0));
 
         System.out.println(spline.driveVector().x);
         System.out.println(spline.driveVector().y);
